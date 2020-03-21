e12bbf2628e72436b648a0030e0a8f8bd0f5bbb1
David Dixon <chimichanga@g.ucla.edu>
2020-03-21 11:35:41 -0700

    add props to list component

--- src/App.js
+++ src/App.js
@@ -3,29 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const List = () => {
+const List = (props) => {
   return (
     <div>
-      {list.map(
+      {props.list.map(
         (item) =>
           <div key={item.objectID}>
             <span>
@@ -41,6 +22,26 @@ const List = () => {
 }
 
 const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = (event) => console.log(event.target.value)
   
   return (
@@ -52,7 +53,7 @@ const App = () => {
 
       <hr/>
 
-      <List/>
+      <List list={stories}/>
 
     </div>
   );

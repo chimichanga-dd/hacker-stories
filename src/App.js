9e0bc40f734a402a8322319e29de9fcea134af9e
David Dixon <chimichanga@g.ucla.edu>
2020-03-21 11:47:50 -0700

    add state modifier and refactor Search to a component

--- src/App.js
+++ src/App.js
@@ -21,6 +21,24 @@ const List = (props) => {
   )
 }
 
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+  
+  return(
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
 
   const stories = [
@@ -42,18 +60,15 @@ const App = () => {
     },
   ];
 
-  const handleChange = (event) => console.log(event.target.value)
-  
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
-
+      <Search />
       <hr/>
 
       <List list={stories}/>
+      
 
     </div>
   );

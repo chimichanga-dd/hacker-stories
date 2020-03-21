4e4f1c99ba621bc0743028c2406ee6f86d5e61a3
David Dixon <chimichanga@g.ucla.edu>
2020-03-21 10:59:33 -0700

    add list

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function getTitle(title){
   return title
 }
-
 
 function App() {
 
@@ -18,6 +36,21 @@ function App() {
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr/>
+
+      {list.map( 
+        (item) =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+      )}
+
     </div>
   );
 }
